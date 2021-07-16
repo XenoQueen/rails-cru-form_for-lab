2071c58cc4033ffc660b6eb9c738890051b4366d
XenoQueen <queenmab051@gmail.com>
2021-07-15 22:35:26 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,71 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'songs/new'
+
+  get 'songs/create'
+
+  get 'songs/edit'
+
+  get 'songs/update'
+
+  get 'songs/destroy'
+
+  get 'songs/index'
+
+  get 'songs/show'
+
+  get 'genres/new'
+
+  get 'genres/create'
+
+  get 'genres/edit'
+
+  get 'genres/update'
+
+  get 'genres/destroy'
+
+  get 'genres/index'
+
+  get 'genres/show'
+
+  get 'artists/new'
+
+  get 'artists/create'
+
+  get 'artists/edit'
+
+  get 'artists/update'
+
+  get 'artists/destroy'
+
+  get 'artists/index'
+
+  get 'artists/show'
+
+  get 'songs/dashboard'
+
+  get 'songs/stats'
+
+  get 'songs/financials'
+
+  get 'songs/settings'
+
+  get 'genres/dashboard'
+
+  get 'genres/stats'
+
+  get 'genres/financials'
+
+  get 'genres/settings'
+
+  get 'artists/dashboard'
+
+  get 'artists/stats'
+
+  get 'artists/financials'
+
+  get 'artists/settings'
+
+  resources :songs
+  resources :genres
+  resources :artists
 end
